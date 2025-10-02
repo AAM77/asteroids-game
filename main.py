4a61eb4e7bd1732221be048d2abcadad684acc9f
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2025-10-02 19:15:29 -0400

    Add grouping

--- main.py
+++ main.py
@@ -13,17 +13,28 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     
+    player.containers = (drawable, updatable)
+    drawable.add(player)
+    updatable.add(player)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for to_draw in drawable:
+            to_draw.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60)/1000.0  # Limit to 60 FPS and get delta time
-        player.update(dt)
+        
+        for to_update in updatable:
+            to_update.update(dt)
     
 
 if __name__ == "__main__":
