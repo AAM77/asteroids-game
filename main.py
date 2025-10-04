be930020abdf5eb8f0eb175cc59113c44cdefc38
Mohammad Adeel <Adeel.A.Mhd@gmail.com>
2025-10-03 23:26:43 -0400

    Add asteroid field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -6,35 +8,46 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
-    clock = pygame.time.Clock()
-    dt = 0
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     drawable = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     
-    player.containers = (drawable, updatable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable)
+    Player.containers = (drawable, updatable)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+    
     drawable.add(player)
     updatable.add(player)
+    updatable.add(asteroid_field)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        updatable.update(dt)
         screen.fill((0, 0, 0))
         
         for to_draw in drawable:
             to_draw.draw(screen)
             
         pygame.display.flip()
-        dt = clock.tick(60)/1000.0  # Limit to 60 FPS and get delta time
         
-        for to_update in updatable:
-            to_update.update(dt)
+        # Limit to 60 FPS and get delta time
+        dt = clock.tick(60)/1000.0
+        
+        # for to_update in updatable:
+        #     to_update.update(dt)
     
 
 if __name__ == "__main__":
